690c097dfefc08fcb9f92feec3d3bf534a36b3fa
lugard1 <agulugard@gmail.com>
2022-12-12 14:05:52 +0100

    update

--- src/index.js
+++
@@ -1,4 +0,0 @@
-import displayTasks from './modules/displayTasks.js';
-import './style.css';
-
-displayTasks();
--- src/modules/store.js
+++
@@ -1,7 +0,0 @@
-const taskArray = JSON.parse(localStorage.getItem('taskInput')) || [];
-
-const store = () => {
-  localStorage.setItem('taskInput', JSON.stringify(taskArray));
-};
-
-export { taskArray, store };
